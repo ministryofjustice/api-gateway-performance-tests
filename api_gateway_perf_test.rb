62e88b635bbf2441c9bcd439bd7c56e1f2ffeed2
Al Davidson <apdavidson@gmail.com>
2017-05-15 10:11:42 +0100

    add batches, to allow emulation of syscon perf testing method

--- api_gateway_perf_test.rb
+++ api_gateway_perf_test.rb
@@ -4,33 +4,47 @@ require 'byebug'
 
 require_relative './gen_auth'
 require_relative './command_line_parser'
-require_relative './httparty_adapter'
-require_relative './api_request'
+require_relative './request_group'
 require_relative './responses_printer'
+require_relative './batches'
 
 BASE_URL              = ENV['NOMIS_API_BASE_URL'] || 'https://noms-api-preprod.dsd.io/nomisapi'
-ENDPOINT_1            = "#{BASE_URL}/health".freeze
-ENDPOINT_2            = "#{BASE_URL}/foobar".freeze
-
-puts opts = CommandLineParser.parse_opts
+ENDPOINT_1            = "#{BASE_URL}/foobar".freeze
 
 threads = []
 responses = []
 
+OPTS = CommandLineParser.parse_opts
+
 ResponsesPrinter.print_heading
 
-opts[:concurrent_users].times.each do |_|
+chosen_batches = OPTS[:batches] || ['default']
+batches_to_run = chosen_batches.map { |b| BATCHES[b.to_sym] }.flatten.compact
+# allow default batch to take some measures from the cmd line param
+default_batch_opts = {
+  number_of_users: OPTS[:concurrent_users],
+  url: (OPTS[:url] || ENDPOINT_1),
+  interval_between_requests: OPTS[:interval_between_requests].to_i,
+  interval_between_users: OPTS[:rampup_per_user].to_i
+}
+
+# start of main loop
+batches_to_run.each do |batch_config|
+  # create a thread for each batch
+  this_batch = default_batch_opts.merge(batch_config)
+  batch = RequestGroup.new(this_batch)
+
   threads << Thread.new do
-    result = ApiRequest.get(url: ENDPOINT_1,
-                            headers: { Authorization: GenAuth.run })
-    responses << result
-    ResponsesPrinter.print(result)
+    puts "starting batch #{batch.label}"
+    # each batch creates one thread per user
+    batch.run(threads, responses)
   end
-  sleep(opts[:rampup_per_user])
 end
 
 threads.each(&:join)
 
 all_responses = responses.flatten
 
+puts "\n\n"
+puts '-----------------------'
 ResponsesPrinter.print_statistics(all_responses)
