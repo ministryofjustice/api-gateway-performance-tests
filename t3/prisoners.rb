5a2253a4871bfafef4a70c271c8d2c14e192cc98
Al Davidson <apdavidson@gmail.com>
2017-10-31 14:32:56 +0000

    correct prisoner_id -> nomis_id

--- t3/prisoners.rb
+++ t3/prisoners.rb
@@ -1,8 +1,8 @@
 PRISONERS = [
-  { prisoner_id: "G4027GP", prison_id: "FNI", dob: "1990-06-03", offender_id: "1470212" },
-  { prisoner_id: "G4805UP", prison_id: "CFI", dob: "1976-03-16", offender_id: "2558337" },
-  { prisoner_id: "G0351UK", prison_id: "LLI", dob: "1973-12-16", offender_id: "2299755" },
-  { prisoner_id: "G4591GU", prison_id: "WLI", dob: "1992-02-13", offender_id: "1061713" },
-  { prisoner_id: "G5507UO", prison_id: "WYI", dob: "1999-05-27", offender_id: "2493791" },
-  { prisoner_id: "G0682VU", prison_id: "LEI", dob: "1988-10-17", offender_id: "1617488" }
+  { nomis_id: "G4027GP", prison_id: "FNI", dob: "1990-06-03", offender_id: "1470212" },
+  { nomis_id: "G4805UP", prison_id: "CFI", dob: "1976-03-16", offender_id: "2558337" },
+  { nomis_id: "G0351UK", prison_id: "LLI", dob: "1973-12-16", offender_id: "2299755" },
+  { nomis_id: "G4591GU", prison_id: "WLI", dob: "1992-02-13", offender_id: "1061713" },
+  { nomis_id: "G5507UO", prison_id: "WYI", dob: "1999-05-27", offender_id: "2493791" },
+  { nomis_id: "G0682VU", prison_id: "LEI", dob: "1988-10-17", offender_id: "1617488" }
 ]
