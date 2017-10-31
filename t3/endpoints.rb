1f9b8cb31462986b52172f9de55e5fc8500e9da3
Al Davidson <apdavidson@gmail.com>
2017-10-31 14:36:55 +0000

    chane NOMIS_ID to OFFENDER_ID on visit urls

--- t3/endpoints.rb
+++ t3/endpoints.rb
@@ -6,9 +6,9 @@ ENDPOINTS = [
   { endpoint: "#{BASE_URL}/offenders/events?prison_id=PRISON_ID&from_datetime=DATETIMEENCODED", method: :get },
   { endpoint: "#{BASE_URL}/lookup/active_offender?date_of_birth=DOB&noms_id=NOMIS_ID", method: :get },
   { endpoint: "#{BASE_URL}/prison/PRISON_ID/slots?start_date=2017-11-01&end_date=2017-11-09", method: :get },
-  { endpoint: "#{BASE_URL}/offenders/NOMIS_ID/visits/available_dates", method: :get },
-  { endpoint: "#{BASE_URL}/offenders/NOMIS_ID/visits/restrictions", method: :get },
-  { endpoint: "#{BASE_URL}/offenders/NOMIS_ID/visits/contact_list", method: :get },
+  { endpoint: "#{BASE_URL}/offenders/OFFENDER_ID/visits/available_dates", method: :get },
+  { endpoint: "#{BASE_URL}/offenders/OFFENDER_ID/visits/restrictions", method: :get },
+  { endpoint: "#{BASE_URL}/offenders/OFFENDER_ID/visits/contact_list", method: :get },
   { endpoint: "#{BASE_URL}/offenders/events/case_notes_for_delius?from_datetime=DATETIMEISO", method: :get },
   { endpoint: "#{BASE_URL}/prison/PRISON_ID/offenders/NOMIS_ID/transactions/", method: :post, body: { type: "MRPR", description: "Misc credit", amount: 1, client_transaction_id: "1234", client_unique_ref: "foobar" } },
 ]
